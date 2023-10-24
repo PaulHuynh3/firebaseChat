ddf2c347459e33fdcd7d5310142fa9e89d185c77
Paul <paul.huynh3@gmail.com>
2023-10-24 15:43:55 -0400

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Paul Huynh on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
