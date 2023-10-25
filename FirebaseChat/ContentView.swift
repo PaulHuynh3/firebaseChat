edbfe6ed7894617104f9dc1daa04dfc96783a745
Paul <paul.huynh3@gmail.com>
2023-10-25 13:01:49 -0400

    Create the name, password textfields and login buttons

--- FirebaseChat/ContentView.swift
+++ FirebaseChat/ContentView.swift
@@ -8,16 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
+    @State var password = ""
+    @State var size: CGSize = .zero
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                VStack(spacing: 25) {
+                    VStack(spacing: 5) {
+                        TextField("Name", text: $name)
+                            .font(.system(size: 14, weight: .regular))
+                            .frame(width: size.width - 50, height: 50)
+                            .padding(.horizontal, 10)
+                            .border(.black)
+                            .background(.white)
+                        
+                        TextField("Password", text: $password)
+                            .font(.system(size: 14, weight: .regular))
+                            .frame(width: size.width - 50, height: 50)
+                            .padding(.horizontal, 10)
+                            .border(.black)
+                            .background(.white)
+                    }
+                    
+                    Button {
+                        //action
+                    } label: {
+                        Text("Log in")
+                            .font(.system(size: 14, weight: .regular))
+                            .foregroundStyle(.white)
+                        
+                    }
+                    .frame(width: size.width - 50, height: 50)
+                    .padding(.horizontal, 10)
+                    .background(.blue)
+                    
+//                    Spacer()
+                }
+                
+                GeometryReader { proxy in
+                    HStack {}
+                        .onAppear {
+                            size = proxy.size
+                        }
+                }
+                
+                
+            }
+            .navigationTitle("Log in")
+            .background(.purple)
+            
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
