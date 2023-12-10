c7fa194d3abf3b26a3ad4adf571a7ad955457802
Paul <paul.huynh3@gmail.com>
2023-12-10 00:33:51 -0500

    Template for mainMessagesView

--- FirebaseChat/MainMessagesView/MainMessagesView.swift
+++ FirebaseChat/MainMessagesView/MainMessagesView.swift
@@ -10,9 +10,85 @@ import SwiftUI
 struct MainMessagesView: View {
     var body: some View {
         NavigationView {
-            ScrollView {
+            VStack {
+                //Custom Nav bar
+                VStack {
+                    HStack {
+                        Image(systemName: "person")
+                            .font(.system(size: 34, weight: .heavy))
+                        
+                        VStack(alignment: .leading ,spacing: 4) {
+                            Text("UserName")
+                                .font(.system(size: 24, weight: .bold))
+                            HStack {
+                                Circle()
+                                    .foregroundColor(.green)
+                                    .frame(width: 12, height: 12)
+                                Text("Online")
+                                    .font(.system(size: 12))
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                        
+                        Spacer()
+                        Image(systemName: "gear")
+                            .font(.system(size: 15))
+                    }
+                    Divider()
+                }
+                .padding(.horizontal)
                 
-            }
+                
+                ScrollView {
+                    ForEach(0..<10, id: \.self) { num in
+                        VStack {
+                            HStack(spacing: 25) {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 30))
+                                    .padding(6)
+                                    .overlay(RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color.black, lineWidth: 1))
+                                
+                                
+                                VStack(alignment: .leading) {
+                                    Text("UserName")
+                                        .font(.system(size: 24, weight: .bold))
+                                    Text("Message sent to user")
+                                        .font(.system(size: 14))
+                                        .foregroundStyle(.gray)
+                                }
+                                
+                                Spacer()
+                                Text("22d")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            Divider()
+                                .padding(.vertical, 8)
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("+ New Message")
+                        Spacer()
+                    }
+                }
+                .foregroundColor(.white)
+                .padding(.vertical)
+                .background(.blue)
+                .cornerRadius(32)
+                .padding(.horizontal)
+                
+                
+
+                
+               
+            }.navigationBarHidden(true)
         }
     }
 }
