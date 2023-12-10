237a7c951248b29cdff7d4975cce2f22dd7e32cd
Paul <paul.huynh3@gmail.com>
2023-12-09 23:53:10 -0500

    Creating template messages View folder

---
+++ FirebaseChat/MainMessagesView/MainMessagesView.swift
@@ -0,0 +1,22 @@
+//
+//  MainMessagesView.swift
+//  FirebaseChat
+//
+//  Created by Paul Huynh on 2023-12-09.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MainMessagesView()
+}
